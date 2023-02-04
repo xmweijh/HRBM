f010400e4da098522c70e5928cbddd7a175af757
xmweijh <xmweijh@foxmail.com>
2023-02-04 15:27:56 +0800

    员工列表页面的基本布局和结构

--- src/main.js
+++ src/main.js
@@ -15,25 +15,10 @@ import router from './router'
 import '@/icons' // icon
 import '@/permission' // permission control
 import * as directives from '@/directives'
+import Component from '@/components'
+Vue.use(Component) // 注册自己的插件
 
-/**
- * If you don't want to use mock-server
- * you want to use MockJs for mock api
- * you can execute: mockXHR()
- *
- * Currently MockJs will be used in the production environment,
- * please remove it before going online ! ! !
- */
-// if (process.env.NODE_ENV === 'production') {
-//   const { mockXHR } = require('../mock')
-//   mockXHR()
-// }
-
-// set ElementUI lang to EN
 Vue.use(ElementUI, { locale })
-// 如果想要中文版 element-ui，按如下方式声明
-// Vue.use(ElementUI)
-
 Vue.config.productionTip = false
 
 Object.keys(directives).forEach(key => {
