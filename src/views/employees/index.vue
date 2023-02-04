38362e727bc862239bdf17e166ffd456384dce8e
xmweijh <xmweijh@foxmail.com>
2023-02-04 15:32:48 +0800

    员工列表数据请求和分页加载

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -10,15 +10,15 @@
         </template>
       </page-tools>
       <!-- 放置表格和分页 -->
-      <el-card>
-        <el-table border>
-          <el-table-column label="序号" sortable="" />
-          <el-table-column label="姓名" sortable="" />
-          <el-table-column label="工号" sortable="" />
-          <el-table-column label="聘用形式" sortable="" />
-          <el-table-column label="部门" sortable="" />
-          <el-table-column label="入职时间" sortable="" />
-          <el-table-column label="账户状态" sortable="" />
+      <el-card v-loading="loading">
+        <el-table border :data="list">
+          <el-table-column label="序号" sortable="" type="index" />
+          <el-table-column label="姓名" sortable="" prop="username" />
+          <el-table-column label="工号" sortable="" prop="workNumber" />
+          <el-table-column label="聘用形式" sortable="" prop="formOfEmployment" />
+          <el-table-column label="部门" sortable="" prop="departmentName" />
+          <el-table-column label="入职时间" sortable="" prop="timeOfEntry" />
+          <el-table-column label="账户状态" sortable="" prop="enableState" />
           <el-table-column label="操作" sortable="" fixed="right" width="280">
             <template>
               <el-button type="text" size="small">查看</el-button>
@@ -32,7 +32,13 @@
         </el-table>
         <!-- 分页组件 -->
         <el-row type="flex" justify="center" align="middle" style="height: 60px">
-          <el-pagination layout="prev, pager, next" />
+          <el-pagination
+            layout="prev, pager, next"
+            :page-size="page.size"
+            :current-page="page.page"
+            :total="page.total"
+            @current-change="changePage"
+          />
         </el-row>
       </el-card>
     </div>
@@ -40,8 +46,36 @@
 </template>
 
 <script>
+import { getEmployeeList } from '@/api/employees'
 export default {
+  data() {
+    return {
+      loading: false,
+      list: [], // 接数据的
+      page: {
+        page: 1, // 当前页码
+        size: 10,
+        total: 0 // 总数
+      }
 
+    }
+  },
+  created() {
+    this.getEmployeeList()
+  },
+  methods: {
+    changePage(newPage) {
+      this.page.page = newPage
+      this.getEmployeeList()
+    },
+    async getEmployeeList() {
+      this.loading = true
+      const { total, rows } = await getEmployeeList(this.page)
+      this.page.total = total
+      this.list = rows
+      this.loading = false
+    }
+  }
 }
 </script>
 
